259320fd1e5083d09e0805f3ed1f2affbefbc73d
Vladimir <likholetov.vladimir2@yandex.ru>
2024-05-19 00:29:08 +0300

    modify the Slide module in the script file

--- js/script.js
+++ js/script.js
@@ -368,10 +368,10 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   next.addEventListener('click', () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == changeToNumber(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += changeToNumber(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -389,9 +389,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click', () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = changeToNumber(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= changeToNumber(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -412,7 +412,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = changeToNumber(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -434,4 +434,6 @@ window.addEventListener('DOMContentLoaded', () => {
     dots.forEach(dot => dot.style.opacity = '.5');
     dots[slideIndex - 1].style.opacity = 1;
   }
+
+  const changeToNumber = (str) => +str.replace(/\D/g, '');
 });
